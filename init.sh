dc29accb7acf7f7cdb8d02df3ff0e1d03d5b59db
avis <aao.t@yandex.ru>
2016-05-19 21:34:31 +0300

    fix

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/gunicorn.conf
+
+sudo /etc/init.d/gunicorn restart
+
