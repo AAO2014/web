3636edb0a167519662001b051526cb772346da6c
avis <aao.t@yandex.ru>
2016-05-20 22:48:49 +0300

    fix

--- hello.py
+++ hello.py
@@ -1,12 +1,14 @@
 def wa(env, start_response):
     import re
-    status = '200 OK'
-    headers = [
-        ('content-type', 'text/plain'),
-    ]
     resp = env['QUERY_STRING'].split("&")
     r = ''
     for i in resp:
         r += i + "\n"
+    status = '200 OK'
+    headers = [
+        ('content-type', 'text/plain'),
+        ('content-lenght', len(r)),
+    ]
+
     start_response(status, headers)
     return [ r ]
