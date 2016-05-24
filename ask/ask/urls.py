cbfacb68e4c94ff304493d5e05285ce4321a65f2
avis <aao.t@yandex.ru>
2016-05-24 21:24:47 +0300

    django

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,16 +3,16 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 
-from qa.views import question
+from qa.views import question, index, login, signup, ask, popular, new
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'qa.views.test'),
-    url(r'^login/$', 'qa.views.test'),
-    url(r'^signup/$', 'qa.views.test'),
+    url(r'^$', index),
+    url(r'^login/$', login),
+    url(r'^signup/$', signup),
     url(r'^question/\d+/$', question),
-    url(r'^ask/.*$', 'qa.views.test'),
-    url(r'^popular/$', 'qa.views.test'),
-    url(r'^new/$', 'qa.views.test'),
+    url(r'^ask/.*$', ask),
+    url(r'^popular/$', popular),
+    url(r'^new/$', new),
 )
