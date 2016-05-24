ca035e692a83acceb4865a00556145299ee15778
avis <aao.t@yandex.ru>
2016-05-24 21:17:36 +0300

    django

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -2,7 +2,9 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-import qa.views import question
+
+from qa.views import question
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
